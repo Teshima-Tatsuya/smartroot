b397938d8eb435fa191ca0d6e4319a696f526e4d
kazuki <kn0831@gmial.com>
2016-01-14 23:02:00 +0900

    タブでレイアウトを追加

---
+++ SmartRoot/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmartRoot
+//
+//  Created by kazuki nishimura on 2016/01/13.
+//  Copyright © 2016年 kazuki nishimura. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SmartRoot/Mypage.swift
@@ -0,0 +1,24 @@
+//
+//  mypage.swift
+//  SmartRoot
+//
+//  Created by kazuki nishimura on 2016/01/14.
+//  Copyright © 2016年 kazuki nishimura. All rights reserved.
+//
+
+import UIKit
+
+class MypageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
