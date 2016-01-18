7e9ac372fa8cd26049e05bb580126437433bc04b
kazuki <kn0831@gmial.com>
2016-01-19 00:11:27 +0900

    pagemenuうまくいかない

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by kazuki nishimura on 2016/01/18.
+//
+//
+
+import Foundation
--- SmartRoot/SecondViewController.swift
+++ SmartRoot/SecondViewController.swift
@@ -17,34 +17,63 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("test")
+        // MARK: - UI Setup
         
-        self.title = "PageMenu"
+        self.title = "PAGE MENU"
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
         self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orangeColor()]
         
-        // 表示するコントローラー
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Array to keep track of controllers in page menu
         var controllerArray : [UIViewController] = []
         
-        let controller1 = self.storyboard?.instantiateViewControllerWithIdentifier("test1")
-        let controller2 = self.storyboard?.instantiateViewControllerWithIdentifier("test2")
+        // Create variables for all view controllers you want to put in the
+        // page menu, initialize them, and add each to the controller array.
+        // (Can be any UIViewController subclass)
+        // Make sure the title property of all view controllers is set
+        // Example:
+        let controller1 : UIViewController = UIViewController(nibName: "TableViewController", bundle: nil)
+        controller1.title = "SAMPLE TITLE"
+        controllerArray.append(controller1)
         
-        controllerArray.append(controller1!)
-        controllerArray.append(controller2!)
+        let controller2 : UIViewController = UIViewController(nibName: "TableViewController", bundle: nil)
+        controller2.title = "SAMPLE TITLE"
+        controllerArray.append(controller2)
+        
+        let controller3 : UIViewController = UIViewController(nibName: "TableViewController", bundle: nil)
+        controller3.title = "SAMPLE TITLE"
+        controllerArray.append(controller3)
+        
+        let controller4 : UIViewController = UIViewController(nibName: "TableViewController", bundle: nil)
+        controller4.title = "SAMPLE TITLE"
+        controllerArray.append(controller4)
+        
+        // Customize page menu to your liking (optional) or use default settings by sending nil for 'options' in the init
+        // Example:
+        let parameters: [CAPSPageMenuOption] = [
+            .ScrollMenuBackgroundColor(UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)),
+            .ViewBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
+            .SelectionIndicatorColor(UIColor.orangeColor()),
+            .BottomMenuHairlineColor(UIColor(red: 70.0/255.0, green: 70.0/255.0, blue: 80.0/255.0, alpha: 1.0)),
+            .MenuItemFont(UIFont(name: "HelveticaNeue", size: 13.0)!),
+            .MenuHeight(80.0),
+            .MenuItemWidth(90.0),
+            .CenterMenuItems(true)
+        ]
+        
+        // Initialize page menu with controller array, frame, and optional parameters
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
         
         
-        let parameters: [String: AnyObject] = ["scrollMenuBackgroundColor": backColor,
-            "viewBackgroundColor": backColor,
-            "selectionIndicatorColor": UIColor.orangeColor(),
-            "bottomMenuHairlineColor": backColor,
-            "menuItemFont": UIFont(name: "HelveticaNeue", size: 13.0)!,
-            "menuHeight": 80.0,
-            "menuItemWidth": 120.0,
-            "centerMenuItems": true,
-            "menuItemSeparatorWidth": 4.3   ]
-        
-        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 50.0, self.view.frame.width, self.view.frame.height), options: parameters)
-        
+        self.addChildViewController(pageMenu!)
         self.view.addSubview(pageMenu!.view)
+        
+        pageMenu!.didMoveToParentViewController(self)
         
     }
 
