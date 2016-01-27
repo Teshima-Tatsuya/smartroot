6706345edaee5c9a8a97193b145b8be3fb1d5500
Shougo Hamada <mame150@gmail.com>
2016-01-27 07:04:01 -0800

    【ルート検索】テキストフィールドの中身を結果のラベルに受け渡せるように ルート検索結果からルート検索画面に戻ることができるように。

--- SmartRoute/SmartRoute/SearchRouteController.swift
+++ SmartRoute/SmartRoute/SearchRouteController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class SearchRouteController: UIViewController {
     
+    @IBOutlet weak var RouteTextField1: UITextField!
+    
+    
+    @IBOutlet weak var RouteTextField2: UITextField!
+    
+    @IBOutlet weak var RouteTextField3: UITextField!
+    
+    @IBOutlet weak var RouteTextField4: UITextField!
+    
+    @IBOutlet weak var RouteTextField5: UITextField!
+    
+    @IBOutlet weak var RouteTextField6: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +33,26 @@ class SearchRouteController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    /*
+    * 画面遷移
+    */
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        let SearchRouteResult:SearchRouteResultController = segue.destinationViewController as! SearchRouteResultController
+        
+        SearchRouteResult.Route1 = RouteTextField1.text!
+        SearchRouteResult.Route2 = RouteTextField2.text!
+        SearchRouteResult.Route3 = RouteTextField3.text!
+        SearchRouteResult.Route4 = RouteTextField4.text!
+        SearchRouteResult.Route5 = RouteTextField5.text!
+        SearchRouteResult.Route6 = RouteTextField6.text!
+        
+    }
+    
+    /*
+    * SearchRouteResultControllerから戻ってきた時の処理
+    */
+    @IBAction func backFromSecondView(segue:UIStoryboardSegue){
+        NSLog("SearchRouteController#backSearchRouteResultController")
+    }
     
 }
--- SmartRoute/SmartRoute/SearchRouteResultController.swift
+++ SmartRoute/SmartRoute/SearchRouteResultController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class SearchRouteResultController: UIViewController {
     
+  
+    @IBOutlet weak var RouteResult1: UILabel!
+    
+    @IBOutlet weak var RouteResult2: UILabel!
+    @IBOutlet weak var RouteResult3: UILabel!
+    @IBOutlet weak var RouteResult4: UILabel!
+    
+    var Route1:String = ""
+    var Route2:String = ""
+    var Route3:String = ""
+    var Route4:String = ""
+    var Route5:String = ""
+    var Route6:String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.RouteResult1.text = self.Route1
+        self.RouteResult2.text = self.Route2
+        self.RouteResult3.text = self.Route3
+        self.RouteResult4.text = self.Route4
+        
     }
     
     override func didReceiveMemoryWarning() {
