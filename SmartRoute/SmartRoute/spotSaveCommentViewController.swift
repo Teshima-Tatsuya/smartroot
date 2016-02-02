9408ee27dfd2db7de357f4633debfb6ceed067e0
kazuki <kn0831@gmial.com>
2016-02-03 00:20:29 +0900

    spotの保存のフォームを追加。画面遷移も実装。後日保存とキーボードの制御を実装

--- SmartRoute/SmartRoute/spotSaveCommentViewController.swift
+++ SmartRoute/SmartRoute/spotSaveCommentViewController.swift
@@ -1,29 +1,25 @@
 //
-//  spotSaveViewController.swift
+//  spotSaveCommentViewController.swift
 //  SmartRoute
 //
-//  Created by kazuki nishimura on 2016/02/03.
+//  Created by kazuki nishimura on 2016/02/02.
 //  Copyright © 2016年 kazuki nishimura. All rights reserved.
 //
 
 import UIKit
 
-class spotSaveViewController: UIViewController {
+class spotSaveCommentViewController: UIViewController {
     
-    @IBOutlet var spotComment: UITextView!
+    @IBOutlet var spotSaveComment: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func unwindToSpotSave(segue:UIStoryboardSegue){
-        
     }
     
     
