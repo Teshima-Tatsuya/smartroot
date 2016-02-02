5637bfd62689904d187f21f772a1282e82e9e81b
kazuki <kn0831@gmial.com>
2016-02-02 22:11:23 +0900

    アイコンの追加とスポットでのviewを追加

---
+++ SmartRoute/SmartRoute/spotSaveViewController.swift
@@ -0,0 +1,29 @@
+//
+//  spotSave.swift
+//  SmartRoute
+//
+//  Created by kazuki nishimura on 2016/02/01.
+//  Copyright © 2016年 kazuki nishimura. All rights reserved.
+//
+
+import UIKit
+
+class spotSaveViewController: UIViewController {
+    
+    @IBOutlet var spotTitle: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let 
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
