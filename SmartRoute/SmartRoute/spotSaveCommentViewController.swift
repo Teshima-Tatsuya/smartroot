8ea6122dac536800a30888c6f5f0e7c413c0281e
kazuki <kn0831@gmial.com>
2016-02-13 23:59:09 +0900

    キーボードの制御を追加

--- SmartRoute/SmartRoute/spotSaveCommentViewController.swift
+++ SmartRoute/SmartRoute/spotSaveCommentViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class spotSaveCommentViewController: UIViewController {
+class spotSaveCommentViewController: UIViewController, UITextViewDelegate{
     
     @IBOutlet var spotSaveComment: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.spotSaveComment.delegate = self 
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
     }
     
     
